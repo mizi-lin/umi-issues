6ad7dc4206ea36b48e0ee41add5524708dc4237a
amily4555 <amily4555@gmail.com>
2020-03-04 09:06:02 +0800

    fix: css 因导入外部资源 字体类的错误 -(relative-path)

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,22 @@
-const config = {};
+import * as path from 'path';
 
-export default config;
+export default {
+  dynamicImport: {},
+  pwa: false,
+  hash: true,
+  alias: {
+    '@': path.resolve(__dirname, 'src'),
+  },
+  define: { 'process.env.PROJECT': 'demo', 'process.env.MRI_ENV': 'dev' },
+  proxy: {
+    '/services': {
+      target: 'http://58.215.174.164:16800/',
+      changeOrigin: true,
+      pathRewrite: { '^/services': '' },
+    },
+  },
+  theme: { 'primary-color': '#1890ff' },
+  chainWebpack: (memo, args) => {
+    memo.module.rule('images').test(/\.(png|jpe?g|gif|webp|eot|woff|woff2|ttf)(\?.*)?$/);
+  },
+};
